2d44409360542585ad41da39853c9bd98b620d4e
OleksiiGoida <48295004+OleksiiGoida@users.noreply.github.com>
2020-10-21 16:59:34 +0300

    Create tasks-02.js

---
+++ tasks-02.js
@@ -0,0 +1,24 @@
+https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+function stringToArray(str){
+	return str.trim().split(" ");
+}
+
+
+https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, "U"); 
+}
+
+https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+var min = function(list){
+    
+    return Math.min(...list);
+}
+
+var max = function(list){
+    
+    return Math.max(...list);
+  }
